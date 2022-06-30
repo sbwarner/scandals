e0f77ac4e417f8e83333628675ac7e1278f4433f
sbwarner <60622478+sbwarner@users.noreply.github.com>
2022-06-30 15:42:08 -0400

    Update analysis.R

--- analysis.R
+++ analysis.R
@@ -116,6 +116,8 @@ VARselect(X_endog_irs, lag.max = 14, type = "const")
 
 # Specify VARs
 
+setseed(2000)
+
 var.beng <- VAR(y = X_endog_beng, p = 7, type="const")
 var.va <- VAR(y = X_endog_va, p = 8, type="const") 
 var.nsa <- VAR(y = X_endog_nsa, p = 7, type="const") 
